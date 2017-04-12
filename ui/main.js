02677e8ee424b18a4a9e207a6d026e605d730d01
HARIKRISHNAN A K <harikrishnan.a.k.c@gmail.com>
2017-04-12 17:30:49 +0530

    Add files via upload

---
+++ ui/main.js
@@ -0,0 +1,41 @@
+console.log('Loaded!');
+
+//var comment=commentInput.value;
+
+var send= document.getElementById('send');
+
+ function hi(){
+					
+					var coment=0,c,prv=0,prvcontent=0;
+					$coment=document.getElementById('com');
+					$c=$coment.value;
+					$prv=document.getElementById('viewcomments');
+					$prvcontent=$prv.innerHTML;
+					var a= new Date();
+					var month = new Array();
+					month[0] = "January";
+					month[1] = "February";
+					month[2] = "March";
+					month[3] = "April";
+					month[4] = "May";
+					month[5] = "June";
+					month[6] = "July";
+					month[7] = "August";
+					month[8] = "September";
+					month[9] = "October";
+					month[10] = "November";
+					month[11] = "December";
+					var mon = month[a.getMonth()];
+					var date=a.getDate();
+					
+					//alert("hello world "+$c);
+					$prvcontent=$prvcontent+`<li>
+									<p><span class="newslist-date">${mon}  ${date}</span>` + $c+ ` </p> <p style="absolute; right:0px;">-- user_name </p>
+								</li>`;
+								
+				    $prv.innerHTML=$prvcontent;
+					$coment.value=null;				
+					console.log("hello world");
+
+				};
+				
